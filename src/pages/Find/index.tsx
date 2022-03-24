8212783677353d3f640e7fe4e1fc72cb2ebf7bc7
SevensCode <419626398@qq.com>
2022-03-24 10:44:24 +0800

    1、新增页面缓存

--- src/pages/Find/index.tsx
+++ src/pages/Find/index.tsx
@@ -1,17 +1,21 @@
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import './index.less';
 import Banner from '@/pages/Find/components/Banner';
 import BannerItem from '@/pages/Find/components/Banner/item';
 import { OtherRequst } from '@/api/other';
+import { KeepAlive } from 'umi';
+
+// 获取轮播图
+const getBanner = async () => {
+    const { banners } = await OtherRequst.getBanner();
+    return banners || [];
+};
 
 const Find: FC = (props) => {
     const [banner, setBanner] = useState<any[]>([]);
-    const getBanner = useCallback(async () => {
-        const { banners } = await OtherRequst.getBanner();
-        setBanner(banners || []);
-    }, []);
+
     useEffect(() => {
-        getBanner();
+        getBanner().then((value) => setBanner(value));
     }, []);
     return (
         <div className="find app-container">
@@ -30,4 +34,10 @@ const Find: FC = (props) => {
     );
 };
 
-export default Find;
+export default () => {
+    return (
+        <KeepAlive saveScrollPosition="screen" name={'find'} when={true}>
+            <Find />
+        </KeepAlive>
+    );
+};
